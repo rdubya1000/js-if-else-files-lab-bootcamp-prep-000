ad8bebf4b29b4d51ee0a404ba8fee2fd94e729f7
rwofford0@gmail.com <rwofford0@gmail.com>
2018-03-29 16:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ var height = "74"
 
 if (name ==="Susan"){
   console.log("The Name is correct")
+} else {
+  console.log("Expected the name to be Susan")
 }
+
+
+
+
 // Don't worry about this
 module.exports = { name, height
 }
